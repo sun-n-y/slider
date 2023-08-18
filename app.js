bd3b51b562a5d138df1113c9c9be76a58e9fbe5d
sunny patel <sunnykpatel.skp@gmail.com>
2023-08-18 14:55:17 -0400

    add slider js complete

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ let intialValue = 0;
 
 nextBtn.addEventListener('click', () => {
   intialValue += 100;
+  prevBtn.style.visibility = 'visible';
+  if ((slides.length - 1) * 100 == intialValue) {
+    nextBtn.style.visibility = 'hidden';
+  }
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${intialValue}%)`;
+  });
+});
+
+prevBtn.addEventListener('click', () => {
+  intialValue -= 100;
+  nextBtn.style.visibility = 'visible';
+  if (intialValue == 0) {
+    prevBtn.style.visibility = 'hidden';
+  }
   slides.forEach((slide) => {
     slide.style.transform = `translateX(-${intialValue}%)`;
   });
